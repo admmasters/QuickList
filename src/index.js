e55a6d1125df8ae2d723fce6421718b13533e3d9
Matt Revell <mattrevell82@me.com>
2017-04-16 20:59:44 +0100

    Add remaining boilerplate files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class Entry extends React.Component<void, void, void> {
+  render() {
+    return <View><Text>Hello Quick List</Text></View>;
+  }
+}
